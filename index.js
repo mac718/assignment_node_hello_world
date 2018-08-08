27987464843a513adfac5f6009ff397364ddbf9d
mac718 <mac718@gmail.com>
2018-08-08 19:09:57 -0400

    create log, info, warning, error functions in logger module

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const chalk = require('chalk');
 var _ = require('lodash');
+var logger = require('./lib/logger')
 
-let arr = [1, 2, 3];
- 
-console.log(chalk.blue('Hello world!'));
-
-_.each(arr, function(num){
-  console.log(num);
-})
+logger.log('hello', 'info');
+logger.info('hello');
+logger.warning('hello');
+logger.error('hello');
